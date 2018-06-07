67472fd337a47a64ed51b609ff4dae7eb1edece4
Qvintvs <emperorview@gmail.com>
2018-06-07 17:49:46 +0800

    populate permissions contract from local json file

--- permissions/permissions_test.go
+++ permissions/permissions_test.go
@@ -16,7 +16,9 @@ const key = `{"address":"ed9d02e382b34818e88b88a309c7fe71e65f419d","crypto":{"ci
 //const enode1 = "ac6b1096ca56b9f6d004b779ae3728bf83f8e22453404cc3cef16a3d9b96608bc67c4b30db88e0a5a6c6390213f7acbe1153ff6d23ce57380104288ae19373ef"
 const enode1 = "abhb1096ca56b9f6d004b779ae3728bf83f8e22453404cc3cef16a3d9b96608bc67c4b30db88e0a5a6c6390213f7acbe1153ff6d23ce57380104288ae19373ef"
 const enode2 = "0ba6b9f606a43a95edc6247cdb1c1e105145817be7bcafd6b2c0ba15d58145f0dc1a194f70ba73cd6f4cdd6864edc7687f311254c7555cc32e4d45aeb1b80416"
-const addr = "0x9d13c6d3afe1721beef56b55d303b09e021e27ab"
+
+// const addr = "0x9d13c6d3afe1721beef56b55d303b09e021e27ab"
+const addr = "0x0000000000000000000000000000000000000020"
 
 // function to test proposeNode
 func TestContract_ProposeNode(t *testing.T) {
@@ -53,7 +55,7 @@ func TestContract_ProposeNode(t *testing.T) {
 	if err != nil {
 		t.Errorf("Failed to propose node: %v", err)
 	}
-	fmt.Printf("Transfer pending: 0x%x\n", tx.Hash())
+	fmt.Printf("Transaction pending: 0x%x\n", tx.Hash())
 }
 
 // function to test getNodeIndex for a given enode
