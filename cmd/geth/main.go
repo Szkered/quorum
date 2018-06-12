fd2ce1823536536a033374d05fcde8297bff20ba
Szkered <emperorview@gmail.com>
2018-06-12 21:54:42 +0800

    inject permissioned-nodes.json into permission contract during node start up

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -18,7 +18,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io/ioutil"
 	"math/big"
@@ -264,11 +263,17 @@ func startNode(ctx *cli.Context, stack *node.Node) {
 	if permissioned := ctx.GlobalBool(utils.EnableNodePermissionFlag.Name); permissioned {
 		log.Trace("Quorum permissioning v2 started")
 
+		var e *eth.Ethereum
+		if err := stack.Service(&e); err != nil {
+			utils.Fatalf("Ethereum service not running: %v", err)
+		}
+
 		rpcClient, err := stack.Attach()
 		if err != nil {
 			utils.Fatalf("Failed to attach to self: %v", err)
 		}
 		stateReader := ethclient.NewClient(rpcClient)
+		log.Trace("rpc connection to permissions contract established")
 
 		datadir := ctx.GlobalString(utils.DataDirFlag.Name)
 
@@ -276,6 +281,7 @@ func startNode(ctx *cli.Context, stack *node.Node) {
 		if err != nil {
 			utils.Fatalf("Failed to read keystore directory: %v", err)
 		}
+		log.Trace("reading account keys...")
 
 		// (zekun) HACK: here we always use the first key as transactor
 		var keyPath string
@@ -287,19 +293,25 @@ func startNode(ctx *cli.Context, stack *node.Node) {
 		if err != nil {
 			utils.Fatalf("Failed to read key file: %v", err)
 		}
-		n := bytes.IndexByte(keyBlob, 0)
+		log.Debug("Finished reading key file", "keyPath", keyPath, "keyBlob", keyBlob)
+		// n := bytes.IndexByte(keyBlob, 0)
+		n := len(keyBlob)
+		log.Debug("Decoding keyBlob", "length", n)
 		key := string(keyBlob[:n])
+		log.Debug("Decoded key", "key", key)
 
 		contractAddr := common.HexToAddress("0x0000000000000000000000000000000000000020") // hard coded in genesis
 		permissionsContract, err := permissions.NewPermissions(contractAddr, stateReader)
 		if err != nil {
 			utils.Fatalf("Failed to instantiate a Permissions contract: %v", err)
 		}
+		log.Debug("Permissions contract instantiated")
 		auth, err := bind.NewTransactor(strings.NewReader(key), "")
 		if err != nil {
 			utils.Fatalf("Failed to create authorized transactor: %v", err)
 		}
-		session := &permissions.PermissionsSession{
+		log.Debug("Transactor created")
+		permissionsSession := &permissions.PermissionsSession{
 			Contract: permissionsContract,
 			CallOpts: bind.CallOpts{
 				Pending: true,
@@ -315,12 +327,17 @@ func startNode(ctx *cli.Context, stack *node.Node) {
 		nodes := p2p.ParsePermissionedNodes(datadir)
 		for _, node := range nodes {
 			enodeID := fmt.Sprintf("%x", node.ID[:])
-			log.Trace("Adding node to permissions contract with enode id: ", enodeID)
-			tx, err := session.ProposeNode(enodeID, true, true)
+			log.Trace("Adding node to permissions contract", "enodeID", enodeID)
+
+			nonce := e.TxPool().Nonce(permissionsSession.TransactOpts.From)
+			permissionsSession.TransactOpts.Nonce = new(big.Int).SetUint64(nonce)
+			log.Trace("Current Nonce", "nonce", nonce)
+
+			tx, err := permissionsSession.ProposeNode(enodeID, true, true)
 			if err != nil {
-				utils.Fatalf("Failed to propose node: %v", err)
+				log.Warn("Failed to propose node", "err", err)
 			}
-			log.Debug("Transaction pending: 0x%x\n", tx.Hash())
+			log.Debug("Transaction pending", "tx hash", tx.Hash())
 		}
 	}
 	//END - QUORUM Permissioning
@@ -372,6 +389,7 @@ func startNode(ctx *cli.Context, stack *node.Node) {
 		if err := stack.Service(&ethereum); err != nil {
 			utils.Fatalf("Ethereum service not running: %v", err)
 		}
+
 		// Use a reduced number of threads if requested
 		if threads := ctx.GlobalInt(utils.MinerThreadsFlag.Name); threads > 0 {
 			type threaded interface {
